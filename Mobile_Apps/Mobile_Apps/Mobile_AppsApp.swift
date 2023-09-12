fe90d5ed762d75ccc1ec9579461c5aa0ddd6dd06
Jasleen <42920356+JasleenSidhu20@users.noreply.github.com>
2023-09-11 20:28:43 -0400

    Xcode project

---
+++ Mobile_Apps/Mobile_Apps/Mobile_AppsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Mobile_AppsApp.swift
+//  Mobile_Apps
+//
+//  Created by Jasleen Sidhu on 9/10/23.
+//
+
+import SwiftUI
+
+@main
+struct Mobile_AppsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
