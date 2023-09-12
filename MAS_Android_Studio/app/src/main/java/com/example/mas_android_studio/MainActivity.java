3c90013955e15994a1f376a572943e0cdada8630
Jasleen <42920356+JasleenSidhu20@users.noreply.github.com>
2023-09-11 22:35:26 -0400

    new activities

--- MAS_Android_Studio/app/src/main/java/com/example/mas_android_studio/MainActivity.java
+++ MAS_Android_Studio/app/src/main/java/com/example/mas_android_studio/MainActivity.java
@@ -3,12 +3,19 @@ package com.example.mas_android_studio;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button register;
+    private Button login;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        register = findViewById(R.id.register);
+        login = findViewById(R.id.login);
     }
 }
